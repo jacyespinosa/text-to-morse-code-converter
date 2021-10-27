25ef7e80a012952b7956f495e97aab3b05bee7cc
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-10-26 19:57:33 -0700

    Added a function that will take the user's input as an argument and use it to convert into morse code.

--- main.py
+++ main.py
@@ -34,3 +34,28 @@ def ask_input():
     user_input = input('Enter text: ').upper()
     user_input = list(user_input)
     return user_input
+
+
+'''Loop through user_input string and convert each character/letter into a Morse Code by matching the key.
+If character is not in the morse_code dictionary, replace it with #
+Space is represented with /'''
+
+
+def converter(input):
+    converted_string = []
+    for char in input:
+        if char == " ":
+            converted_string.append("/")
+        elif char not in morse_code:
+            code = '#'
+            converted_string.append(code)
+        for key, value in morse_code.items():
+            if char == key:
+                code = value
+                converted_string.append(code)
+
+    result = " ".join(converted_string)
+    return result
+
+
+print(converter(ask_input()))
